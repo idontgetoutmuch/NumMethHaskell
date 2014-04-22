bee06b5cd68e3e2c6aea0b4ea3c9907145a1cac1
Dominic Steinitz <dominic@steinitz.org>
2014-04-22 17:11:37 +0100

    A working version for a single temperature. We now really need some pictures.

--- TSP.hs
+++ TSP.hs
@@ -1,13 +1,23 @@
-import Prelude hiding ( zipWith, sum )
+{-# OPTIONS_GHC -Wall                      #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing   #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults    #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind   #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods  #-}
+{-# OPTIONS_GHC -fno-warn-orphans          #-}
+
+module TSP ( test ) where
+
+import Prelude hiding ( zipWith, sum, foldl, foldr, scanl, length, null, tail, zip, map, reverse, concat, take )
 import qualified Prelude as P
-import Data.Vector.Unboxed
-import Data.Vector.Unboxed.Mutable
+import Data.Vector.Unboxed hiding ( replicateM )
+import Data.Random.Source.PureMT
+import Data.Random
+import Control.Monad.State hiding ( modify )
+
+import Debug.Trace
 
 nNodes :: Int
-nNodes = 5
-
-nextNode :: Vector Int
-nextNode = generate nNodes ((`mod` nNodes) . (+1))
+nNodes = 6
 
 xs, ys :: Vector Float
 xs = generate nNodes ((*1.0) . fromIntegral)
@@ -17,18 +27,70 @@ distance :: Int -> Int -> Float
 distance i j = sqrt $ (xs!i - xs!j)^2 + (ys!i - ys!j)^2
 
 totalDistance :: Vector Int -> Float
-totalDistance v = sum $ zipWith distance v (backpermute v nextNode)
-
-swapPair :: Int -> Int -> Vector Int -> Vector Int
-swapPair i j v = modify (\v -> swap v i j) v
-
-incCost :: Int -> Int -> Vector Int -> Float
-incCost i j v = d + c - b - a
+totalDistance v = sum $ map (uncurry distance) ms
   where
-    a = distance i (v!i)
-    b = distance j (v!j)
-    c = distance i j
-    d = distance (v!i) (v!j)
+    ms = zip v (tail v)
+
+reverseBetweenPair :: Int -> Int -> Vector Int -> Vector Int
+reverseBetweenPair i j v = concat [beginning, reverse middle, end]
+  where
+    k = length v
+
+    beginning = slice 0       i           v
+    middle    = slice i       (j - i + 1) v
+    end       = slice (j + 1) (k - j - 1) v
+
+incDistance :: Vector Int -> Int -> Int -> Float
+incDistance v i j = d + c - b - a
+  where
+    a = distance (v!(i - 1)) (v!i)
+    b = distance (v!j)       (v!((j + 1) `mod` nNodes))
+    c = distance (v!(i - 1)) (v!j)
+    d = distance (v!i)       (v!((j + 1) `mod` nNodes))
 
 expDv :: Int -> Int -> Vector Int -> Float -> Float -> Float -> Float
-expDv i1 i2 v kB j t = exp(-j * (incCost i1 i2 v) / (kB * t))
+expDv i1 i2 v kB j t = exp(-j * (incDistance v i1 i2) / (kB * t))
+
+randomUpdates :: Int -> Vector (Int, Int, Float)
+randomUpdates m =
+  fromList $
+  evalState (replicateM m x)
+  (pureMT 1)
+  where
+    x = do r <- sample (uniform (1 :: Int)    (nNodes - 2))
+           c <- sample (uniform (r + 1)       (nNodes - 1))
+           v <- sample (uniform (0 :: Float)           1.0)
+           return (r, c, v)
+
+kB, couplingConstant :: Float
+kB               = 1.0
+couplingConstant = 1.0
+
+data McState = McState {
+  mcRoute :: !(Vector Int)
+  }
+  deriving Show
+
+initMcState :: McState
+initMcState = McState {
+  mcRoute = fromList [0,4,2,3,1,5,0]
+  }
+
+singleUpdate :: Float -> McState -> (Int, Int, Float) -> McState
+singleUpdate t u (i, j, r) =
+  trace (show i P.++ show j P.++ ": " P.++
+         show (totalDistance v) P.++ ", " P.++
+         show (totalDistance (reverseBetweenPair i j v)) P.++ ", " P.++
+         show (incDistance v i j) P.++ ", " P.++
+         show p P.++ ", " P.++
+         show r) $ if incDistance v i j <= 0 || p > r
+  then
+    McState { mcRoute = reverseBetweenPair i j v }
+  else
+    u
+  where
+    v = mcRoute u
+    p = expDv i j v kB couplingConstant t
+
+test :: McState
+test = foldl (singleUpdate 1.0) initMcState (randomUpdates 100)
