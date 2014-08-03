b971fa5524c5a510e0173e538f8464077ca85d7d
Dominic Steinitz <dominic@steinitz.org>
2014-08-03 14:33:52 +0100

    The prediction step explained.

--- KalmanChart.hs
+++ KalmanChart.hs
@@ -7,6 +7,7 @@
 
 module KalmanChart (
     diagPartFilter
+  , diagEsts
   ) where
 
 import Control.Lens hiding ( (#) )
@@ -22,12 +23,12 @@ import System.IO.Unsafe
 denv :: DEnv
 denv = unsafePerformIO $ defaultEnv vectorAlignmentFns 500 500
 
-diagPartFilter :: [(Double, Double)] -> Int -> QDiagram Cairo R2 Any
-diagPartFilter ls n =
-  fst $ runBackend denv (render (chartPartFilter ls n) (500, 500))
+diagPartFilter :: Double -> [(Double, Double)] -> Int -> QDiagram Cairo R2 Any
+diagPartFilter x ls n =
+  fst $ runBackend denv (render (chartPartFilter x ls n) (500, 500))
 
-chartPartFilter :: [(Double, Double)] -> Int -> Renderable ()
-chartPartFilter lineVals n = toRenderable layout
+chartPartFilter :: Double -> [(Double, Double)] -> Int -> Renderable ()
+chartPartFilter x lineVals n = toRenderable layout
   where
 
     fitted = plot_lines_values .~ [lineVals]
@@ -35,9 +36,59 @@ chartPartFilter lineVals n = toRenderable layout
               $ plot_lines_title .~ "Trajectory"
               $ def
 
-    layout = layout_title .~ "Gibbs Sampling Bivariate Normal (" ++ (show n) ++ " samples)"
-           $ layout_y_axis . laxis_generate .~ scaledAxis def (-3,3)
-           $ layout_x_axis . laxis_generate .~ scaledAxis def (0,100)
+    actual = plot_lines_values .~ [zip (map fst lineVals) (repeat x)]
+              $ plot_lines_style  . line_color .~ opaque red
+              $ plot_lines_title .~ "Actual"
+              $ def
 
-           $ layout_plots .~ [toPlot fitted]
+    layout = layout_title .~ "Particle Filtering"
+           $ layout_y_axis . laxis_generate .~ scaledAxis def (-3,3)
+           $ layout_x_axis . laxis_generate .~ scaledAxis def (0,(fromIntegral n))
+
+           $ layout_plots .~ [ toPlot fitted
+                             , toPlot actual
+                             ]
+           $ def
+
+diagEsts :: [(Double, Double)] ->
+            [(Double, Double)] ->
+            [(Double, Double)] ->
+            [(Double, Double)] ->
+            QDiagram Cairo R2 Any
+diagEsts ls us ks ps =
+  fst $ runBackend denv (render (chartEsts ls us ks ps) (500, 500))
+
+chartEsts :: [(Double, Double)] ->
+             [(Double, Double)] ->
+             [(Double, Double)] ->
+             [(Double, Double)] ->
+             Renderable ()
+chartEsts lineVals upperVals lowerVals pointVals = toRenderable layout
+  where
+
+    fitted = plot_lines_values .~ [lineVals]
+              $ plot_lines_style  . line_color .~ opaque blue
+              $ plot_lines_title .~ "Estimate"
+              $ def
+
+    upper  = plot_lines_values .~ [upperVals]
+              $ plot_lines_style  . line_color .~ opaque green
+              $ plot_lines_title .~ "Upper error"
+              $ def
+
+    lower  = plot_lines_values .~ [lowerVals]
+              $ plot_lines_style  . line_color .~ opaque green
+              $ plot_lines_title .~ "Lower error"
+              $ def
+
+    dataPts = plot_points_style .~ filledCircles 2 (opaque red)
+              $ plot_points_values .~ pointVals
+              $ plot_points_title .~ "Samples"
+              $ def
+
+    layout = layout_title .~ "Estimation by Kalman Filtering"
+           $ layout_plots .~ [toPlot fitted,
+                              toPlot upper,
+                              toPlot lower,
+                              toPlot dataPts]
            $ def
